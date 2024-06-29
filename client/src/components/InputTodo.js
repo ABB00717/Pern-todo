043f18c44831f7d4efddff1d850364c89cb2605e
ABB00717 <eric94528@gmail.com>
2024-06-29 14:11:28 +0800

    Add: build the list todo component

--- client/src/components/InputTodo.js
+++ client/src/components/InputTodo.js
@@ -3,19 +3,19 @@ import React, { Fragment, useState } from "react";
 const InputTodo = () => {
   const [description, setDescription] = useState("");
 
-  const onSubmitForm = async (e) => {
+  const onSubmitForm = async e => {
     e.preventDefault();
     try {
       const body = { description };
       const response = await fetch("http://localhost:5000/todos", {
         method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(body)
       });
 
       window.location = "/";
     } catch (err) {
-      console.log(err.message);
+      console.error(err.message);
     }
   };
 
@@ -27,7 +27,7 @@ const InputTodo = () => {
           type="text"
           className="form-control"
           value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          onChange={e => setDescription(e.target.value)}
         />
         <button className="btn btn-success">Add</button>
       </form>
